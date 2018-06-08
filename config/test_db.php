83b219106d6a8b2a88f12d9b9b9db2ec3721f177
S. Simeonov <svilborg@gmail.com>
2018-06-08 17:35:29 +0300

    Tests, Registration unset password hash

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2_basic_tests';
+$db['dsn'] = 'mysql:host=172.20.0.2;dbname=yiiapi';
 
 return $db;
--- models/SignupForm.php
+++ models/SignupForm.php
@@ -29,7 +29,7 @@ class SignupForm extends Model
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['email', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This email address has already been taken.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
@@ -53,6 +53,10 @@ class SignupForm extends Model
         $user->setPassword($this->password);
         $user->generateAuthKey();
 
-        return $user->save() ? $user : null;
+        $user = $user->save() ? $user : null;
+
+        unset($user->password_hash);
+
+        return $user;
     }
 }
